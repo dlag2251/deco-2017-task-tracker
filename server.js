4f10fee3f9465f22aef928233adf74d3bc06a38e
Dina Lagacy <dll228@cornell.edu>
2023-06-02 04:22:14 -0400

    changed file structure

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const app = express();
 
-app.use(express.static(__dirname + '/dist'))
+app.use(express.static(__dirname + '/dist'));
 
 app.get('/', function(req, res){
     res.sendFile(__dirname + '/dist/index.html');
